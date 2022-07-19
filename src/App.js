1f181388268f988cc9a993f0607f72230a7d7855
HakobSargsyan94 <hakobsargsyan94@gmail.com>
2022-07-19 22:18:23 +0400

    first commit react

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from "react";
+import Counter from "./components/Counter";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [value, setValue] = useState('Input text');
+
+
+
+    return (
+        <div className="App">
+            <div className="post">
+                <div className="post_content">
+                    <strong>1. Java script</strong>
+                    <div>
+                        Java script is language of development
+                    </div>
+                </div>
+                <div className="post_btns">
+                    <button>Delete</button>
+                </div>
+            </div>
+        </div>);
 }
 
 export default App;
